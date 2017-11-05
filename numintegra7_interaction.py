8efaeaf3ee386a63248c3035c395192b76f186c8
Roo <belredbel@googlemail.com>
2017-11-05 21:45:16 +1100

    clarifying results of interaction terms
    Removing function with 'not adding up' continuity equations
    Correcting remaining function to make continuity equations add up
    improving clarity of reading code by spacing out math equation
    Improve readibility of comments according to python standards
    Add time quivalence marker on Omega plot
    Clarifying x axis labels

--- numintegra7_interaction.py
+++ numintegra7_interaction.py
@@ -49,123 +49,158 @@ NOT AN ASSIGNMENT
 
 from scipy.integrate import odeint
 import numpy as np
-from pylab import figure, plot, xlabel, grid, legend, title
+from pylab import figure, plot, scatter, xlabel, grid, legend, title, annotate
 from matplotlib.font_manager import FontProperties
 
 #def vectorfield(v, t, w):
-#    a, a_dot, e_dash_m, e_dash_de, omega0m, omega0de = v
-#    w_m, w_de = w
+#    """
+#    Takes in    v = values at time now
+#                w = omega parameters
+#                lamb = interaction constant.
+#                
+#    Returns a function with     a_dot, a_dotdot, 
+#                                e'_dotm, e'_dotde, 
+#                                omegam_dot, omegade_dot,
+#    ready to be integrated with odeint.
+#    Uses different lambdas for different fluids.
+#    """
+#    (a, a_dot, e_dash_m, e_dash_de, omega0m, omega0de) = v
+#    (w_m, w_de) = w
+#    
 #    lamb_m = -2e-2 #10
 #    lamb_de = 2e-2
+#    
 #    # Create f = [a_dot, a_dotdot, e'_dotm, e'_dotde, omegam_dot, omegade_dot]:
-#    f = [a_dot, 
-#         (-a/2)*(e_dash_m*(1+3*w_m)+e_dash_de*(1+3*w_de)), 
-#         -3*(a_dot/a)*(e_dash_m*(1+w_m+lamb_m/3*a/a_dot)),
-#         -3*(a_dot/a)*(e_dash_de*(1+w_de+lamb_de/3*a/a_dot)),
-#         (H0**2)*e_dash_m*(2*a/a_dot)*(1-a*(-a/2)*(e_dash_m*(1+3*w_m)+
-#                           e_dash_de*(1+3*w_de))/a_dot**2)+(a/a_dot)**2*
-#                           (-3*a_dot/a*e_dash_m*(1+w_m)),
-#         (H0**2)*e_dash_de*(2*a/a_dot)*(1-a*(-a/2)*(e_dash_m*(1+3*w_m)+
-#                            e_dash_de*(1+3*w_de))/a_dot**2)+(a/a_dot)**2*
-#                            (-3*a_dot/a*e_dash_de*(1+w_de))]
+#    f = [# a_dot
+#         a_dot,
+#         # a_dotdot
+#         (-a/2) * (e_dash_m * (1+3*w_m) + e_dash_de * (1+3*w_de)),
+#         # e'_dotm
+#         -3 * (a_dot/a) * (e_dash_m * (1 + w_m + lamb_m/3 * a/a_dot)),
+#         # e'_dotde
+#         -3 * (a_dot/a) * (e_dash_de * (1 + w_de + lamb_de/3 * a/a_dot)),
+#         # omegam_dot
+#         (H0**2) * e_dash_m * (2*a/a_dot) * (1 - a * (-a/2) 
+#             * (e_dash_m * (1+3*w_m) + e_dash_de * (1+3*w_de)) 
+#             / a_dot**2) + (a/a_dot)**2 * (-3 * a_dot/a * e_dash_m * (1+w_m)),
+#        # omegade_dot
+#         (H0**2) * e_dash_de * (2*a/a_dot) * (1 - a * (-a/2) 
+#             * (e_dash_m * (1+3*w_m) + e_dash_de * (1+3*w_de)) 
+#             / a_dot**2) + (a/a_dot)**2 * (-3 * a_dot/a * e_dash_de * (1+w_de))
+#         ]
 #        
 #    return f
 
-def vectorfield(v, t, w):
-    a, a_dot, e_dash_m, e_dash_de, omega0m, omega0de = v
-    w_m, w_de = w
-    lamb = 2e-1 #10
+def vectorfield(v, t, w, lamb):
+    """
+    Takes in    v = values at time now
+                w = omega parameters
+                lamb = interaction constant.
+                
+    Returns a function with     a_dot, a_dotdot, 
+                                e'_dotm, e'_dotde, 
+                                omegam_dot, omegade_dot,
+    ready to be integrated with odeint.
+    Uses same lambdas for all fluids.
+    """
+    (a, a_dot, e_dash_m, e_dash_de, omegam, omegade) = v
+    (w_m, w_de) = w
+    
     # Create f = [a_dot, a_dotdot, e'_dotm, e'_dotde, omegam_dot, omegade_dot]:
-    f = [a_dot, 
-         (-a/2)*(e_dash_m*(1+3*w_m)+e_dash_de*(1+3*w_de)), 
-         -3*(a_dot/a)*(e_dash_m*(1+w_m-lamb/3*a/a_dot)),
-         -3*(a_dot/a)*(e_dash_de*(1+w_de+lamb/3*a/a_dot)),
-         (H0**2)*e_dash_m*(2*a/a_dot)*(1-a*(-a/2)*(e_dash_m*(1+3*w_m)+
-                           e_dash_de*(1+3*w_de))/a_dot**2)+(a/a_dot)**2*
-                           (-3*a_dot/a*e_dash_m*(1+w_m)),
-         (H0**2)*e_dash_de*(2*a/a_dot)*(1-a*(-a/2)*(e_dash_m*(1+3*w_m)+
-                            e_dash_de*(1+3*w_de))/a_dot**2)+(a/a_dot)**2*
-                            (-3*a_dot/a*e_dash_de*(1+w_de))]
+    f = [# a_dot
+         a_dot,
+         # a_dotdot
+         (-a/2) * (e_dash_m * (1+3*w_m) + e_dash_de * (1+3*w_de)), 
+         # e'_dotm
+         -3 * (a_dot/a) * (e_dash_m * (1 + w_m -lamb/3 * a/a_dot 
+              * e_dash_de/e_dash_m )),
+         # e'_dotde
+         -3 * (a_dot/a) * (e_dash_de * (1 + w_de +lamb/3 * a/a_dot)),
+         # omegam_dot
+         (H0**2) * e_dash_m * (2*a/a_dot) * (1 - a * (-a/2) 
+             * (e_dash_m * (1+3*w_m) + e_dash_de * (1+3*w_de)) 
+             / a_dot**2) + (a/a_dot)**2 * (-3 * a_dot/a * e_dash_m * (1+w_m)),
+         # omegade_dot        
+         (H0**2) * e_dash_de * (2*a/a_dot) * (1 - a * (-a/2) 
+             * (e_dash_m * (1+3*w_m) + e_dash_de * (1+3*w_de)) 
+             / a_dot**2) + (a/a_dot)**2 * (-3 * a_dot/a * e_dash_de * (1+w_de))
+         ]
         
     return f
 
-# a past which to discard values, value chosen by looking at the plot 
-# set arbitrarily - sometimes jumps over the result(?)
+
+# Parameters (script specific)
+    
+# Interaction term, rate at which DE decays into matter.
+lamb = 5e-1
+
+# Last value for a before results are to be considered 
+# invalid due to close proximity to big bang, a_d is set
+# somewhat arbitrarily - sometimes jumps over the result(?).
 a_d = 10e-6 
 
-# time in 1/H0 to integrate until, if this time isn't long enough to reach a_d
-# then "time" will be added to integration time until a_d is reached
-# 0.665 matter only, 0.96 m+de, -0.49
+# Time (in 1/H0) to integrate until.  If this time isn't long enough for a to 
+# decrease to a_d then stoptime will be extended by time until a_d is reached.
+# 0.665 matter only, 0.96 standard m+de
 time = 0.6
 
-# Parameters
+# Standard cosmological parameters.
 H0 = 1       # Hubble parameter at t=now
-#Dh = c/H0   # Hubble distance
 tH = 1.0/H0  # Hubble time
-# G = 1
-# c = 1
-
-# Eq of state parameter
-w_m = 0.0     # matter
-w_de = -1.0   # cosmological constant (dark energy)
+# Eq of state for known fluids:
 w_r = 1/3     # radiation
+w_m = 0.0     # matter
+w_de = -1.0   # cosmological constant (dark energy?)
 
-# Initial conditions
-# a0 = scale factor, a_dot = speed, e_dash0 = e0/ec0
-a0 = 1.0
-a_dot0 = 1.0
-e_dash0m = 0.3
-e_dash0de = 0.7
-omega0m = 0.3
-omega0de = 0.7
+# Initial conditions at time = t0.
+a0 = 1.0        # scale factor
+a_dot0 = 1.0    # speed of expansion
+e_dash0m = 0.3  # e_m(t)/ec(t0)
+e_dash0de = 0.7 # e_de(t)/ec(t0)
+omega0m = 0.3   # e_m(t)/ec(t)
+omega0de = 0.7  # e_de(t)/ec(t)
 
-# ODE solver parameters
+# ODE solver parameters:
 abserr = 1.0e-8
 relerr = 1.0e-6
 numpoints = 250
 
-stoptime = -time
-
-# Plot the solution
-figure()
-xlabel('t in 1/H0')
-grid(True)
-lw = 1
+stoptime = -time # Integrating back in time as time now is t0.
 
 while True:
-#    print('stoptime is:',str(stoptime),'1/H0')
-    # Create the time samples for the output of the ODE solver.
-    t = [stoptime*tH * float(i) / (numpoints - 1) for i in range(numpoints)]
+    # Create time samples for the ODE solver.
+    t = [stoptime * tH * float(i) / (numpoints - 1) for i in range(numpoints)]
     
-    # Pack up the parameters and initial conditions:
+    # Pack up the initial conditions and eq of state parameters.
     v0 = [a0, a_dot0, e_dash0m, e_dash0de, omega0m, omega0de]
-    
     w = [w_m, w_de]
     
     # Call the ODE solver.
-    vsol = odeint(vectorfield, v0, t, args=(w,), atol=abserr, rtol=relerr)
+    vsol = odeint(vectorfield, v0, t, args=(w,lamb,), atol=abserr, rtol=relerr)
     
-    # Remove unwanted results from the plot
-    # Separate results into their own arrays
+    # Remove unwanted results which are too close to big bang from the plot.
+    # Separate results into their own arrays:
     a = vsol[:,0]
     a_dot = vsol[:,1]
     e_dashm = vsol[:,2]
     e_dashde = vsol[:,3]
     omegam = vsol[:,4]
     omegade = vsol[:,5]
-    
-    # Find where results start to get strange
-    blowups = np.where(a < a_d)    # tuple with indecies of a
-                                   # so small it blows up a_dot
-    blowups = np.asarray(blowups)  # converting to np array   
 
-    if blowups.any():                       
-        blowup = blowups[0,0]      # first instance of a being too small
-    else: 
-        stoptime -= time
+    
+    # Find where results start to get strange (smaller than a_d):
+    blowups = np.where(a < a_d)    # Tuple with indecies of a so
+                                   # small that other results blow up.                             
+    blowups = np.asarray(blowups)  # Converting to np array.
+
+    if blowups.any():              # Check if instances of a < a_d exist.   
+        blowup = blowups[0,0]
+    else:                          # If no instance of a < a_d was found
+        stoptime -= time           # then integrate further back in time.
         continue
     
-    # Remove the values after the index when a is too small
+    
+    # Remove the values after the index of first instance of a < a_d.
     t_cut = np.asarray(t)
     t_cut = t_cut[:blowup]
     a = a[:blowup]
@@ -174,57 +209,74 @@ while True:
     e_dashde = e_dashde[:blowup]
     omegam = omegam[:blowup]
     omegade = omegade[:blowup]
+    omegatot = np.add(omegam,omegade)
     
-    # Age
+    # Find when density of DE was equal to that of matter.  Rounding the omegas
+    # to avoid not finding an instance of equality.
+    equiv = np.argmin(abs(omegam - omegade))    # Index of time of equivlence.
+    equiv = np.asarray(equiv)                   # Converting to nparray.
+    
+    # Age of the universe.
     age = t_cut[np.argmin(t_cut)]
-    age = round(age, 2)
+    age = -round(age, 2)
     
-    # plotting selected resutls
+    # Plotting selected results:
     # a and a_dot
-    xlabel('t in 1/H0')
-    lw = 1
-    plot(t_cut, a, 'r', linewidth=lw)
-    plot(t_cut, a_dot, 'b', linewidth=lw)
+    figure()
+    xlabel('time in $H_0^{-1}$')
+    grid(True)
+    plot(t_cut, a, 'r', lw=1)
+    plot(t_cut, a_dot, 'b', lw=1)
     legend((r'$a$', r'$\.a$'), prop=FontProperties(size=16))
-    title('Cut results for $\omega$ = %s, age = %s 1/H0'%(w,age))
+    title('Cut results for $\omega$ = %s, $\lambda$ = %s, age = %s $H_0^{-1}$'
+          %(w,lamb,age))
     
-    while False:
+    while False:    # Looped to make it faster to switch plots on and off.
         # e_dashm
         figure()
-        xlabel('t in 1/H0')
-        lw = 1
-        plot(t_cut, e_dashm, 'g', linewidth=lw)
+        xlabel('time in $H_0^{-1}$')
+        plot(t_cut, e_dashm, 'g', lw=1)
         title('Cut $\epsilon_m \'$ for $\omega$ = %s'%(w))
         
         # e_dashde
         figure()
-        xlabel('t in 1/H0')
-        lw = 1
-        plot(t_cut, e_dashde, 'm', linewidth=lw)
+        xlabel('time in $H_0^{-1}$')
+        plot(t_cut, e_dashde, 'm', lw=1)
         title('Cut $\epsilon_{DE} \'$ for $\omega$ = %s'%(w))
         
         break
     
     # omegam and omegade
     figure()
-    plot(t_cut, omegam, 'c', linewidth=lw)
-    plot(t_cut, omegade, 'k', linewidth=lw)
-    legend((r'$\Omega_m$', r'$\Omega_{DE}$'), prop=FontProperties(size=16))
-    title('Cut results for $\omega$ = %s, age = %s 1/H0'%(w,age))
+    xlabel('time in $H_0^{-1}$')
+    grid(True)
+    plot(t_cut, omegam, 'c', lw=1)
+    plot(t_cut, omegade, 'k', lw=1)
+    plot(t_cut, omegatot, 'tab:orange', ls= 'dashed', lw=1, 
+         markevery=np.int(equiv))
+    scatter(t_cut[equiv], omegade[equiv], s=80, 
+            facecolors='none', edgecolors='r')
+    legend((r'$\Omega_m$', r'$\Omega_{DE}$', r'$\Omega_{(m + DE)}$', 
+            r'$t_{m = DE}$'), prop=FontProperties(size=16))
+    annotate('$t_{m = DE}$ = %s $H_0^{-1}$'%(round(t_cut[equiv], 4)), 
+            xy=(t_cut[equiv], 0.45), 
+            xytext=(-0.35, np.min(omegade)+0.03), 
+            arrowprops = dict(facecolor='white', shrink=0.05))
+    title('Cut results for $\omega$ = %s, $\lambda$ = %s, age = %s $H_0^{-1}$'
+          %(w,lamb,age))
     
     break
         
 
-
+# Complete results with blow up resulting from a approaching big bang point.
 while False:  
     figure()
-    xlabel('t in 1/H0')
+    xlabel('time in $H_0^{-1}$')
     grid(True)
-    lw = 1
     
-    # plotting all results
-    plot(t, vsol[:,0], 'r', linewidth=lw)
-    plot(t, vsol[:,1], 'b', linewidth=lw)
+    # Plotting complete results.
+    plot(t, vsol[:,0], 'r', lw=1)
+    plot(t, vsol[:,1], 'b', lw=1)
     
     legend((r'$a$', r'$\.a$', r'$\'\epsilon$'), prop=FontProperties(size=16))
     title('Complete results for $\omega$ = %s'%(w))
