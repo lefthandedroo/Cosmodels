944bcdf2059b49ac0caae83d8a2c67a6211049c0
Roo <belredbel@googlemail.com>
2019-01-15 23:44:51 +1100

    in prep for meeting, fixing scolnic plots

--- Models/res_sn.py
+++ Models/res_sn.py
@@ -9,20 +9,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import datasim
 
-data_name = 'synth'
+#data_name = 'synth'
 data_name = 'pantheon'
 
 if data_name == 'pantheon':
+    print('-----Using pantheon')
     # Pantheon data:
     import pandas as pd
     pantheon = pd.read_csv('./data/lcparam_full_long.txt', sep=" ")
     # Stacking arrays together and sorting by accending redshift.
     data = np.stack((pantheon.mb.values,pantheon.zhel.values), axis=0)
     data.sort(axis=-1)
-    pmag = data[0]
+    mag = data[0]
     zpicks = data[-1]
-    data_dic = {'mag':pmag, 'zpicks':zpicks}
+    data_dic = {'mag':mag, 'zpicks':zpicks}
 elif data_name == 'synth':
+    print('-----Using artificial data')
     # Loading artificial LCDM SN Ia data:
     from pathlib import Path
     import pickle
@@ -31,6 +33,61 @@ elif data_name == 'synth':
     if my_file.is_file():
         with open(dataname,'rb') as rfp: zpicks, mag = pickle.load(rfp)
     data_dic = {'mag':mag, 'zpicks':zpicks}
+
+scolnic_Mcorr_zpicks = [
+0.30012
+,0.14761
+,0.22853
+,0.1388
+,0.14343
+,0.09201
+,0.1568
+,0.23851
+,0.14949
+,0.50718
+,0.25249
+,0.28397
+,0.2368
+,0.34092
+,0.26388]
+
+
+scolnic_Mcorr = [
+21.71
+,19.89
+,20.97
+,19.82
+,19.88
+,18.95
+,20.12
+,21.20
+,20.15
+,22.99
+,21.21
+,21.32
+,21.02
+,21.89
+,21.06]
+
+scolnic_data_mag = np.zeros(len(scolnic_Mcorr))
+
+i=0
+for z in scolnic_Mcorr_zpicks:
+    index = np.argmax(zpicks == z)
+    if index == 0:
+        print(f"Didn't find an exact z = {z} match")
+        index = np.argmax(zpicks > z)
+        print(f'but found closest z = {zpicks[index]}')
+#    print(index)
+    scolnic_data_mag[i] = mag[index]
+    i+=1
+
+plt.figure()
+plt.title(f'SN Ia mag from Scolnic and mag at Scolnic paper redshifts')
+plt.scatter(scolnic_Mcorr_zpicks, scolnic_Mcorr, label='scolnic paper mag')
+plt.scatter(scolnic_Mcorr_zpicks, scolnic_data_mag, label=f'{data_name} data at scolnic z')
+plt.legend()
+
 
 
 # LCDM model magnitude and da
@@ -66,122 +123,63 @@ elif data_name == 'synth':
 #mag, da = datasim.magn(names, values, data_dic, test_key, plot_key=False)
 
 
-test_key1 = 'stepfall'
-sample = np.loadtxt(f'./results_Bfactor/{data_name}/2_model_{test_key1}/sample.txt')
-sample_info = np.loadtxt(f'./results_Bfactor/{data_name}/2_model_{test_key1}/sample_info.txt')
-transposed_sample_info = sample_info.transpose()
-maxlike_index = np.argmax(transposed_sample_info[1])
-
-names = ['Mcorr', 'matter', 'radiation', 'a_ombar', 'v_in', 'w_in', 'x_in']
-values = sample[maxlike_index,:]
-mag1, da1 = datasim.magn(names, values, data_dic, test_key1, plot_key=False)
-
-
-#test_key0 = 'LCDM'
 test_key0 = 'rLCDM'
-sample = np.loadtxt(f'./results_Bfactor/{data_name}/2_model_{test_key0}/sample.txt')
-sample_info = np.loadtxt(f'./results_Bfactor/{data_name}/2_model_{test_key0}/sample_info.txt')
-transposed_sample_info = sample_info.transpose()
-maxlike_index = np.argmax(transposed_sample_info[1])
+sample0 = np.loadtxt(f'./results_Bfactor/{data_name}/2_model_{test_key0}/sample.txt')
+sample_info0 = np.loadtxt(f'./results_Bfactor/{data_name}/2_model_{test_key0}/sample_info.txt')
+transposed_sample_info0 = sample_info0.transpose()
+maxlike_index0 = np.argmax(transposed_sample_info0[1]) # The 1st (staring column 0th) in sample_info is the log-likelihood value.
+names0 = ['Mcorr', 'm_ombar']
+values0 = sample0[maxlike_index0,:]
+#print('values0',values0)
+mag0, da0 = datasim.magn(names0, values0, data_dic, test_key0, plot_key=False)
 
-names = ['Mcorr', 'm_ombar']
-values = sample[maxlike_index,:]
-mag0, da0 = datasim.magn(names, values, data_dic, test_key0, plot_key=False)
+test_key1 = 'stepfall'
+sample1 = np.loadtxt(f'./results_Bfactor/{data_name}/2_model_{test_key1}/sample.txt')
+sample_info1 = np.loadtxt(f'./results_Bfactor/{data_name}/2_model_{test_key1}/sample_info.txt')
+transposed_sample_info1 = sample_info1.transpose()
+maxlike_index1 = np.argmax(transposed_sample_info1[1])
+names1 = ['Mcorr', 'matter', 'radiation', 'a_ombar', 'v_in', 'w_in', 'x_in']
+values1 = sample1[maxlike_index1,:]
+#print('values1',values1)
+mag1, da1 = datasim.magn(names1, values1, data_dic, test_key1, plot_key=False)
 
+scolnic_omega_m = [
+0.304
+,0.317
+,0.289
+,0.281
+,0.301
+,0.307]
 
+for omega_m in scolnic_omega_m:
+    test_key00 = 'LCDM'
+    names00 = ['Mcorr', 'm_ombar']
+    values00 = [-19.3, omega_m]
+    #print('values00',values00)
+    mag00, da00 = datasim.magn(names00, values00, data_dic, test_key00, plot_key=False)
 
+    ## SN Ia plots:
+    #plt.figure()
+    #plt.title(f'SN Ia magnitudes, data = {data_name} '+'\n Noise parameters: $\mu = 0.0$, $\sigma = 0.07$')
+    #plt.xlabel('z')
+    #plt.ylabel('Mag')
+    #plt.scatter(zpicks, data_dic['mag'], label=data_name, marker=',', s=1)
+    #plt.plot(zpicks, mag0, label=test_key0)
+    #plt.plot(zpicks, mag1, label=test_key1)
+    #plt.plot(zpicks, mag00, label=test_key00)
+    #plt.legend()
 
+    # Residuals:
+    plt.figure()
+    plt.title(f'SN Ia magnitude residuals, data = {data_name}')
+    plt.ylabel('Mag')
+    plt.xlabel('z')
+    #plt.xlim(0.5,1)
+    plt.scatter(zpicks, data_dic['mag'] - mag00, label=f'{data_name} SN Ia data - scolnic omega_m {test_key00}', marker=',', s=1)
+    plt.scatter(zpicks, mag0 - mag1, label=f'DNest {test_key0} - DNest {test_key1}', marker=',', s=1)
+    plt.scatter(zpicks, mag00 - mag1, label='LCDM w scolnic omega_m - DNest {test_key1}', marker=',', s=1)
+    plt.grid(True)
+    plt.legend()
 
-
-# SN Ia plots:
-plt.figure()
-plt.title(f'SN Ia magnitudes, data = {data_name} '+'\n Noise parameters: $\mu = 0.0$, $\sigma = 0.07$')
-plt.xlabel('z')
-plt.ylabel('Mag')
-plt.scatter(zpicks, data_dic['mag'], label=data_name, marker=',', s=1)
-plt.plot(zpicks, mag0, label=test_key0)
-plt.plot(zpicks, mag1, label=test_key1)
-plt.legend()
-
-# Residuals:
-plt.figure()
-plt.title(f'SN Ia magnitude residuals, data = {data_name}')
-plt.ylabel('Mag')
-plt.xlabel('z')
-#plt.xlim(0.5,1)
-plt.scatter(zpicks, data_dic['mag'] - mag0, label=f'{data_name} SN Ia data - DNest {test_key0}', marker=',', s=1)
-plt.scatter(zpicks, mag0 - mag1, label=f'DNest {test_key0} - DNest {test_key1}', marker=',', s=1)
-#plt.scatter(zpicks, mbest_LCDM_diff, label='LCDM - DNest {test_key0}', marker=',', s=1)
-plt.grid(True)
-plt.legend()
-
-approx_corr_zpicks = [
-0.30012
-,0.14761
-,0.22853
-,0.1388
-,0.14343
-,0.09201
-,0.1568
-,0.23851
-,0.14949
-,0.50718
-,0.25249
-,0.28397
-,0.2368
-,0.34092
-,0.26388]
-
-corr_zpicks = []
-
-Mcorr = [
-21.71
-,19.89
-,20.97
-,19.82
-,19.88
-,18.95
-,20.12
-,21.20
-,20.15
-,22.99
-,21.21
-,21.32
-,21.02
-,21.89
-,21.06]
-
-datamag = []
-mag0 = []
-i = 0
-names = ['Mcorr', 'm_ombar']
-test_key0 = 'LCDM'
-
-for z in approx_corr_zpicks:
-    index = np.argmax(zpicks == z)
-    if index == 0:
-        index = np.argmax(zpicks > z)
-    corr_zpicks.append(zpicks[index])
-    datamag.append(mag[index])
-    values = [Mcorr[i], 0.310]
-    print(values, z)
-    values = np.asarray(values)
-    i+=1
-    data_dic['zpicks'] = z
-    magnitude, da = datasim.magn(names, values, data_dic, test_key0, plot_key=False)
-    mag0.append(magnitude)
-
-datamag = np.asarray(datamag)
-mag0 = np.asarray(mag0)
-mag0 = mag0.flatten()
-# Residuals:
-plt.figure()
-plt.title(f'SN Ia magnitude residuals, data = {data_name}')
-plt.ylabel('Mag')
-plt.xlabel('z')
-#plt.xlim(0.5,1)
-plt.scatter(corr_zpicks, datamag - mag0, label=f'{data_name} SN Ia data - pantheon parameters', marker=',', s=1)
-plt.grid(True)
-plt.legend()
 
 plt.show()
